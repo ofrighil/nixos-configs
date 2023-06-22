68b8e02db3dfdef07b96c804ce606d598fba795b
ofrighil <eugene.ofriel@gmail.com>
2023-06-22 00:11:01 -0400

    remove old files

--- home.nix
+++
@@ -1,9 +0,0 @@
-{
-  # home = rec {
-  #   username = "ofrighil";
-  #   homeDirectory = "/home/${username}";
-  #   stateVersion = "23.11";
-  # };
-  home.stateVersion = "23.11";
-  xdg.enable = true;
-}
--- ofrighil/ofrighil.nix
+++
@@ -1,8 +0,0 @@
-{
-  users.users.ofrighil = {
-    isNormalUser = true;
-    uid = 6349;
-    home = "/home/ofrighil";
-    extraGroups = [ "wheel" "networkmanager" ];
-  };
-}
