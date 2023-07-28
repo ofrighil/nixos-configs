3c9d37473ac347a6dad265e910f1902fbf11d43b
ofrighil <eugene.ofriel@gmail.com>
2023-07-27 21:28:41 -0400

    updated neovim init.lua

--- home/editor/neovim/nvim/init.lua
+++ home/editor/neovim/nvim/init.lua
@@ -3,5 +3,17 @@ vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 
 require('lazyvim')
 
+vim.o.number = true
+vim.o.relativenumber = true
+
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.smartindent = true
+
+vim.o.completeopt = 'menuone,noselect'
 vim.o.clipboard = 'unnamedplus'
-vim.wo.number = true
+vim.o.termguicolors = true
+vim.o.undofile = true
+
