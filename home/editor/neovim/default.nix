ec4f597b1ac532bff869e058f438932f3887ae3d
ofrighil <eugene.ofriel@gmail.com>
2023-07-30 16:29:57 -0400

    updated neovim lsp shortcuts

--- home/editor/neovim/default.nix
+++ home/editor/neovim/default.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }: {
-  home.packages = [ pkgs.neovim ]; #pkgs.gcc ];
+  home.packages = [ pkgs.neovim ];
 
   programs.neovim.plugins = [ pkgs.vimPlugins.nvim-treesitter.withAllGrammars ];
 
--- home/editor/neovim/nvim/after/plugin/lsp.lua
+++ home/editor/neovim/nvim/after/plugin/lsp.lua
@@ -1,11 +1,54 @@
 local lsp = require('lsp-zero').preset({})
 
-lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({buffer = bufnr})
-end)
-
-lsp.setup_servers({'rust_analyzer'})
+lsp.setup_servers({'pylsp', 'rust_analyzer'})
 
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
+lsp.on_attach(
+  function(client, bufnr)
+    local nmap = function(keys, func, desc)
+      if desc then
+	desc = 'LSP: ' .. desc
+      end
+
+      vim.keymap.set(
+        'n', keys, func, { buffer = bufnr, desc = desc, remap = false }
+      )
+    end
+
+    nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+    nmap('rn', vim.lsp.buf.rename, '[R]e[n]ame')
+    nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+
+    nmap('[d', vim.diagnostic.goto_prev, 'Go to previous diagnostic message')
+    nmap(']d', vim.diagnostic.goto_next, 'Go to next diagnostic message')
+    nmap('<leader>vd', vim.diagnostic.open_float, 'Open floating diagnostic message')
+  end
+)
+
 lsp.setup()
+
+local cmp = require('cmp')
+local luasnip = require('luasnip')
+
+luasnip.config.setup()
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      luansip.lsp_expand(args.body)
+    end
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm({select=true}),
+    ['<C-j>'] = cmp.mapping.scroll_docs(4),
+    ['<C-k>'] = cmp.mapping.scroll_docs(-4),
+  }),
+  source = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' }, 
+  },
+})
