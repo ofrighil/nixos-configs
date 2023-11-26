7ca47878cbdf5ea357ae76c054cf7cefec1ffa1b
ofrighil <eugene.ofriel@gmail.com>
2023-11-25 22:56:58 -0500

    specify which nvim repos to freeze

--- home/editor/neovim/default.nix
+++ home/editor/neovim/default.nix
@@ -4,5 +4,7 @@
     pkgs.vimPlugins.nvim-treesitter.withAllGrammars
   ];
 
-  xdg.configFile."nvim/" = { source = ./nvim; };
+  xdg.configFile."nvim/init.lua" = { source = ./nvim/init.lua; };
+  xdg.configFile."nvim/after" = { source = ./nvim/after; };
+  xdg.configFile."nvim/lua" = { source = ./nvim/lua; };
 }
