fa98efc067f1d645b93e4ece4bef563cd12a6cd0
ofrighil <eugene.ofriel@gmail.com>
2023-12-22 17:19:37 -0500

    add nushell config.nu and env.nu

--- home/shell/default.nix
+++ home/shell/default.nix
@@ -1,3 +1,3 @@
 { ... }: {
-  imports = [ ./zsh ];
+  imports = [ ./nushell ./zsh ];
 }
---
+++ home/shell/nushell/default.nix
@@ -0,0 +1,4 @@
+{
+  xdg.configFile."nushell/config.nu".source = ./nushell/config.nu;
+  xdg.configFile."nushell/env.nu".source = ./nushell/env.nu;
+}
