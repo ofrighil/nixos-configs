11aa24dc8d08dcc7774b56ad602e9e647311d5f4
ofrighil <eugene.ofriel@gmail.com>
2024-03-17 18:43:36 -0400

    add BML font

--- secrets.nix
+++ secrets.nix
@@ -1,20 +1,27 @@
 let
   fonts = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEyHG9JyTC3kTOvbpIcvWJN8PlAX35/txp5UkIdw85tu" ];
-  font-mll = "MLL-v1_808";
+  personal_fonts = "./modules/form/fonts/personal";
+  font_bml = "BML-v1_009";
+  font_mll = "MLL-v1_808";
 in
 {
-    "./form/fonts/personal/${font-mll}-Black.ttf.age".publicKeys = fonts;
-    "./form/fonts/personal/${font-mll}-BlackItalic.ttf.age".publicKeys = fonts;
-    "./form/fonts/personal/${font-mll}-Bold.ttf.age".publicKeys = fonts;
-    "./form/fonts/personal/${font-mll}-BoldItalic.ttf.age".publicKeys = fonts;
-    "./form/fonts/personal/${font-mll}-ExtraLight.ttf.age".publicKeys = fonts;
-    "./form/fonts/personal/${font-mll}-ExtraLightItalic.ttf.age".publicKeys = fonts;
-    "./form/fonts/personal/${font-mll}-Light.ttf.age".publicKeys = fonts;
-    "./form/fonts/personal/${font-mll}-LightItalic.ttf.age".publicKeys = fonts;
-    "./form/fonts/personal/${font-mll}-Medium.ttf.age".publicKeys = fonts;
-    "./form/fonts/personal/${font-mll}-MediumItalic.ttf.age".publicKeys = fonts;
-    "./form/fonts/personal/${font-mll}-Regular.ttf.age".publicKeys = fonts;
-    "./form/fonts/personal/${font-mll}-RegularItalic.ttf.age".publicKeys = fonts;
-    "./form/fonts/personal/${font-mll}-Thin.ttf.age".publicKeys = fonts;
-    "./form/fonts/personal/${font-mll}-ThinItalic.ttf.age".publicKeys = fonts;
+    "${personal_fonts}/${font_bml}-Bold.ttf.age".publicKeys = fonts;
+    "${personal_fonts}/${font_bml}-BoldItalic.ttf.age".publicKeys = fonts;
+    "${personal_fonts}/${font_bml}-Italic.ttf.age".publicKeys = fonts;
+    "${personal_fonts}/${font_bml}-Regular.ttf.age".publicKeys = fonts;
+
+    "${personal_fonts}/${font_mll}-Black.ttf.age".publicKeys = fonts;
+    "${personal_fonts}/${font_mll}-BlackItalic.ttf.age".publicKeys = fonts;
+    "${personal_fonts}/${font_mll}-Bold.ttf.age".publicKeys = fonts;
+    "${personal_fonts}/${font_mll}-BoldItalic.ttf.age".publicKeys = fonts;
+    "${personal_fonts}/${font_mll}-ExtraLight.ttf.age".publicKeys = fonts;
+    "${personal_fonts}/${font_mll}-ExtraLightItalic.ttf.age".publicKeys = fonts;
+    "${personal_fonts}/${font_mll}-Light.ttf.age".publicKeys = fonts;
+    "${personal_fonts}/${font_mll}-LightItalic.ttf.age".publicKeys = fonts;
+    "${personal_fonts}/${font_mll}-Medium.ttf.age".publicKeys = fonts;
+    "${personal_fonts}/${font_mll}-MediumItalic.ttf.age".publicKeys = fonts;
+    "${personal_fonts}/${font_mll}-Regular.ttf.age".publicKeys = fonts;
+    "${personal_fonts}/${font_mll}-RegularItalic.ttf.age".publicKeys = fonts;
+    "${personal_fonts}/${font_mll}-Thin.ttf.age".publicKeys = fonts;
+    "${personal_fonts}/${font_mll}-ThinItalic.ttf.age".publicKeys = fonts;
 }
